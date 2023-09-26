f468613aa8f99fac9a05ff5bdffebeab997daf32
Chalvinhuby55 <cjemihuby@gmail.com>
2023-09-26 18:49:33 +0900

    function arguments

---
+++ function-arguments.php
@@ -0,0 +1,12 @@
+<?php
+
+function nama($nama , $alamat ,$umur)
+{
+    echo "Perkenalkan nama saya :" .$nama . " Saya tinggal di  ".$alamat."<br>";
+    echo "Umur Saya".".$umur". "<br>";
+
+};
+
+nama ("Chalvin","Waena",20);
+nama ("Kusaa","Expo",21);
+nama ("KK Sam","Sentani",22);
